aca1a6d937515add3a7340ed18a11d44e971b538
Arunkumar1610 <arunkumarpandian2022@gmail.com>
2023-07-24 13:34:34 +0530

    java-project

--- Password_validation/src/main/java/com/example/demo/password.java
+++ Password_validation/src/main/java/com/example/demo/password.java
@@ -22,31 +22,26 @@ public class password {
 	            boolean isValid = true;
 	            if (password.length() > 15 || password.length() < 8)
 	            {
-	                    System.out.println("Password must be less than 20 and more than 8 characters in length.");
 	                    isValid = false;
 	            }
 	            String upperCaseChars = "(.*[A-Z].*)";
 	            if (!password.matches(upperCaseChars ))
 	            {
-	                    System.out.println("Password must have atleast one uppercase character");
 	                    isValid = false;
 	            }
 	            String lowerCaseChars = "(.*[a-z].*)";
 	            if (!password.matches(lowerCaseChars ))
 	            {
-	                    System.out.println("Password must have atleast one lowercase character");
 	                    isValid = false;
 	            }
 	            String numbers = "(.*[0-9].*)";
 	            if (!password.matches(numbers ))
 	            {
-	                    System.out.println("Password must have atleast one number");
 	                    isValid = false;
 	            }
 	            String specialChars = "(.*[@,#,$,%].*$)";
 	            if (!password.matches(specialChars ))
 	            {
-	                    System.out.println("Password must have atleast one special character among @#$%");
 	                    isValid = false;
 	            }
 				return isValid;
